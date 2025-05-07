fdc72c9a49b0b6f96ae14bb630754933d48de21c
Maximino Peralta <maximino.peralta@freshconsulting.com>
2025-05-07 10:34:34 -0600

    creates the initial Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:banking_flutter_app/config/theme/app_theme.dart';
+import 'package:banking_flutter_app/presentation/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,13 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      theme: AppTheme(selectedColor: 4).getTheme(),
+      home: HomeScreen(),
     );
   }
 }
