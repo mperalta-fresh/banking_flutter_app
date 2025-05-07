ffe3d44905d6dec67659feb430e3feaa1559a887
Maximino Peralta <maximino.peralta@freshconsulting.com>
2025-05-07 10:30:40 -0600

    adds menu items using a List

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,35 @@
+// import 'package:flutter/material.dart' show IconData;
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  /// * constructor
+  const MenuItem({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  
+  MenuItem(
+    title: 'Botones',
+    subtitle: 'Varios botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined
+  ),
+
+  MenuItem(
+    title: 'Tarjetas',
+    subtitle: 'Un contenedor estilizado',
+    link: '/cards',
+    icon: Icons.credit_card
+  ),
+
+];
