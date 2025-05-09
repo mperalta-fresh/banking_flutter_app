c8201c2830569256f1f4ada1afdd710f82406e52
Maximino Peralta <maximino.peralta@freshconsulting.com>
2025-05-09 17:44:16 -0600

    creates new screens and updates barrel file

--- lib/presentation/home/screens/screens.dart
+++ lib/presentation/home/screens/screens.dart
@@ -1,4 +1,10 @@
 // Barrel file
+export 'package:banking_flutter_app/presentation/home/screens/animated/animated_screen.dart';
+export 'package:banking_flutter_app/presentation/home/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:banking_flutter_app/presentation/home/screens/buttons/buttons_screen.dart';
 export 'package:banking_flutter_app/presentation/home/screens/cards/cards_screen.dart';
 export 'package:banking_flutter_app/presentation/home/screens/home/home_screen.dart';
+export 'package:banking_flutter_app/presentation/home/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:banking_flutter_app/presentation/home/screens/progress/progress_screen.dart';
+export 'package:banking_flutter_app/presentation/home/screens/snackbar/snackbar_screen.dart';
+export 'package:banking_flutter_app/presentation/home/screens/ui_controls/ui_controls_screen.dart';
